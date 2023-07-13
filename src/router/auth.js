3ff9a225a5fa0ff161a9ff8eb631d6d52fce42da
afdhalluthfi09 <afdhal.luthfi@student.amikom.ac.id>
2023-07-13 13:58:22 +0700

    nambah file auth

---
+++ src/router/auth.js
@@ -0,0 +1,12 @@
+import  { useRouter } from 'vue-router';
+
+export function requireAuth(to,from,next){
+    const router =useRouter();
+    let isLoggedIn =(localStorage.getItem('token') == null) ? true : false;
+    
+    if(!isLoggedIn){
+        next();
+    }else{
+        router.push('/login');
+    }
+}
