4a4a5de3fc0cbaaebc0949eb782eefd763462768
Afdhal Luthfi <39642502+afdhalluthfi09@users.noreply.github.com>
2023-07-14 15:48:30 +0000

    test

--- src/views/setting/pages/SettingView.vue
+++ src/views/setting/pages/SettingView.vue
@@ -154,7 +154,7 @@
 <script>
 import Swal from 'sweetalert2'
 import { messaging } from '@/firebaseConfig';
-import { getMessaging, getToken } from 'firebase/messaging';
+import { getToken } from 'firebase/messaging';
 export default {
   name: "SettingViewComponent",
   created() { },
@@ -249,31 +249,32 @@ export default {
           }).then(()=>{
             this.closeModal();
             console.log('hei');
-            const messagingInstance = getMessaging(messaging.app);
-            getToken(messagingInstance, {
-              vapidKey: 'BJ3K0Ed4igq-C44zKfCdvtnsIFylEf4rlVNQIUOVrMJtXRy9R3Zip7vXXUXkwuh3pxqRp8yxMAaW_DK-NVKBfl4',
-            }).then((currentToken) => {
-              if (currentToken) {
-                messagingInstance.send({
-                    to: 'cf8LFotjBkwWT4pPKLT1hp:APA91bGAOvL48FvkAxe0KWGHp5GwhzutbCGwI0rIyCo9F0FOnkuPcWpa9KN6IPcKR5UrdSJV7WGqRLI5c5FUv8rhXr0SAS79v8YjbGrcgMjOzE6po065i0c_oGal0J1pE3kGhWnlzsGF',
-                    notification: {
-                      title: 'Notifikasi dari Vue.js',
-                      body: 'Ini adalah pesan notifikasi',
-                    },
-                  })
-                  .then((response) => {
-                    console.log('Notifikasi berhasil dikirim ke Laravel:', response);
-                  })
-                  .catch((error) => {
-                    console.log('Terjadi kesalahan saat mengirim notifikasi:', error);
-                  });
-
-              }else {
-                console.log('Tidak ada token pendaftaran yang tersedia.');
-              }
-            }) .catch((err) => {
-              console.log('Terjadi kesalahan saat mendapatkan token:', err);
-            });
+            getToken(messaging, {
+                vapidKey: 'BJ3K0Ed4igq-C44zKfCdvtnsIFylEf4rlVNQIUOVrMJtXRy9R3Zip7vXXUXkwuh3pxqRp8yxMAaW_DK-NVKBfl4',
+              })
+                .then((currentToken) => {
+                  if (currentToken) {
+                    messaging
+                      .send({
+                        to: 'cf8LFotjBkwWT4pPKLT1hp:APA91bGAOvL48FvkAxe0KWGHp5GwhzutbCGwI0rIyCo9F0FOnkuPcWpa9KN6IPcKR5UrdSJV7WGqRLI5c5FUv8rhXr0SAS79v8YjbGrcgMjOzE6po065i0c_oGal0J1pE3kGhWnlzsGF',
+                        notification: {
+                          title: 'Notifikasi dari Vue.js',
+                          body: 'Ini adalah pesan notifikasi',
+                        },
+                      })
+                      .then((response) => {
+                        console.log('Notifikasi berhasil dikirim ke Laravel:', response);
+                      })
+                      .catch((error) => {
+                        console.log('Terjadi kesalahan saat mengirim notifikasi:', error);
+                      });
+                  } else {
+                    console.log('Tidak ada token pendaftaran yang tersedia.');
+                  }
+                })
+                .catch((err) => {
+                  console.log('Terjadi kesalahan saat mendapatkan token:', err);
+                });
 
           });
         })
